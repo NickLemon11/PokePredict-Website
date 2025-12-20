faadc07a5704a5f067709af1458ab5489d06acd6
NickLemon11 <Nicksolonolo@gmail.com>
2025-12-20 10:34:54 -0500

    credits-guide

--- src/GuidePage.jsx
+++ src/GuidePage.jsx
@@ -4,7 +4,11 @@ import "./guide.css";
 export default function Guide() {
     return (
     <>
-    <div className="main-content" id="guide">GUIDE</div>
-    </>
+    <div className="main-content" id="guide">1. Enter each of your pokemon into team viewer and enter the pokemons and moves of the opponent that you know. <br/> 
+                                             2. Left click on a stat to increase it and adjust it so each Pokemon matches the stats in the battle. <br/>
+                                             3. Switch view and enter the number of turns you want PokePredict to predict, along with the active Pokemon and its status condition. <br/>
+                                             4. Enter the terrain and weather conditions, then click 'calculate', to proceed to next turn click 'next'.
+                                             </div>
+    </>             
     );
 }
